2fa6aa7cbcfa6beafe1411183962b4566aa3078d
Igor Jair Laurindo Ribeiro <igorjairlaurindoribeiro@gmail.com>
2024-05-20 10:55:19 -0300

    Delete src directory

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express"
-
-const router = Router()
-
-router.post('/login', async (req, res) => {
-  const { username, password } = req.body
-})
-
-export default router
--- src/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router, json } from "express"
-import userRouter from "./user"
-
-const router = Router()
-router.use(json())
-router.use('/user', userRouter)
-router.get('/', (req, res) => res.send('Hello World!'))
-export default router
